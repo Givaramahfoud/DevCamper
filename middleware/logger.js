e72c32b1e1013113f3281a8355e4c35caf56e31d
givara <givoo25@gmail.com>
2020-06-19 22:24:03 +0200

    setup middelware

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,4 @@
+// an example for creating your onw middleware 
 const logger = (req, res, next) => {
     console.log(`${req.method} ${req.protocol}://${req.get('host')} ${req.originalUrl}`);
     next()
